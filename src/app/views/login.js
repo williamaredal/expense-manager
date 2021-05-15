9c999de139a2aa672211fd020bf791886b5150ad
William Peter Åredal <williamaredal@hotmail.com>
2021-05-15 14:21:21 +0200

    moved css and added register and login views

--- src/app/views/login.js
+++ src/app/views/login.js
@@ -1,11 +1,80 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import './login.css';
 
-export default function Login () {
+const mapStateToProps = (state) => {
+    return{
+        currentState : state
+    }
+}
+
+
+function Login () {
+
+    const [redirectState, updateRedirect] = useState({
+        redirect : false,
+        path : '',
+    });
+
     const [isAuthenticated, authenticate] = useState(false);
+    const [loginDetails, updateDetails] = useState({
+        username : '',
+        password : '',
+    });
+
+    function updateFormVars (key, value) {
+        updateDetails({
+            ...loginDetails,
+            [key] : value,
+        });
+    }
+
+    function loginUser () {
+        console.log('user sign in trial with these details: ', loginDetails);
+        console.log('if authenticated the user was redirected to dashboard page');
+        // openPage('/')
+
+    }
+    
+    function openPage (currentPath) {
+        updateRedirect({
+            redirect : true,
+            path : currentPath,
+        });
+    }
 
     return (
-        <div>
-            <p>{isAuthenticated.toString()}</p>
+        <div className="mainView">
+            <div className="loginTitle">
+                <h1>
+                    Sign in
+                </h1>
+            </div>
+
+            <div className="loginFormCard">
+
+                {redirectState.redirect ? <Redirect exact to={redirectState.path} /> : null}
+                <form className="loginForm">
+                    
+                    <label className="formLabel">
+                        Username:
+                        <input className="formInput" required name="username" type="text" onChange={(e) => updateFormVars(e.target.name, e.target.value)}></input>
+                    </label>   
+
+                    <label className="formLabel">
+                        Password:
+                        <input className="formInput" required name="password" type="password" onChange={(e) => updateFormVars(e.target.name, e.target.value)}></input>
+                    </label>
+
+                    <button className="button" onClick={() => loginUser()}>Sign in</button>
+                    <button className="button" onClick={() => openPage('/register')}>Register</button>                    
+
+                </form>
+
+            </div>
         </div>
     )
 }
+
+export default connect(mapStateToProps)(Login);
--- src/app/views/register.js
+++ src/app/views/register.js
@@ -1,13 +1,85 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import './registration.css';
 
-export default function Register () {
+const mapStateToProps = (state) => {
+    return{
+        currentState : state
+    }
+}
+
+
+function Register () {
+
+    const [redirectState, updateRedirect] = useState({
+        redirect : false,
+        path : '',
+    });
+
     const [isAuthenticated, authenticate] = useState(false);
+    const [registrationDetails, updateDetails] = useState({
+        email : '',
+        username : '',
+        password : '',
+    });
+
+    function updateFormVars (key, value) {
+        updateDetails({
+            ...registrationDetails,
+            [key] : value,
+        });
+    }
+
+    function registerUser () {
+        console.log('user was registered with these details: ', registrationDetails);
+        console.log('then the user was redirected to login page');
+        // openPage('/login')
+
+    }
+    
+    function openPage (currentPath) {
+        updateRedirect({
+            redirect : true,
+            path : currentPath,
+        });
+    }
 
     return (
-        <div>
-            <p>
-                Register {isAuthenticated.toString()}
-            </p>
+        <div className="mainView">
+            <div className="registrationTitle">
+                <h1>
+                    Register
+                </h1>
+            </div>
+
+            <div className="registrationFormCard">
+
+                {redirectState.redirect ? <Redirect exact to={redirectState.path} /> : null}
+                <form className="registrationForm">
+                    
+                    <label className="formLabel">
+                        Email:
+                        <input className="formInput" required name="email" type="email" onChange={(e) => updateFormVars(e.target.name, e.target.value)}></input>
+                    </label>
+                    
+                    <label className="formLabel">
+                        Username:
+                        <input className="formInput" required name="username" type="text" onChange={(e) => updateFormVars(e.target.name, e.target.value)}></input>
+                    </label>   
+
+                    <label className="formLabel">
+                        Password:
+                        <input className="formInput" required name="password" type="password" onChange={(e) => updateFormVars(e.target.name, e.target.value)}></input>
+                    </label>
+                    
+                    <button className="button" onClick={() => registerUser()}>Register</button>
+                    <button className="button" onClick={() => openPage('/login')}>Sign in</button>
+                </form>
+
+            </div>
         </div>
     )
 }
+
+export default connect(mapStateToProps)(Register);
